34ab5840f755a48c9b16f94098d935b1309fd889
richard wetterauer <rhwette2022@gmail.com>
2022-11-13 08:43:32 -0600

    Nov13 0843

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
   return (
     <div className="footer">
-      <p className="footer-name"> Developed by Practicum student Name rhw</p>
+      <p className="footer-name"> Developed by Practicum student - rhw</p>
       <p className="footer-date"> 2022</p>
     </div>
   );
