31e498668b19a1fc41e3770516fd5b32767188dd
richard wetterauer <rhwette2022@gmail.com>
2022-11-26 12:16:16 -0600

    Nov 26 1216 itemModal close working

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -7,6 +7,7 @@
 
 import React from 'react';
 import './ItemModal.css';
+import closeX from '../../images/close.svg';
 
 function ItemModal({ card, onClose }) {
   const src2 = card.link;
@@ -18,6 +19,12 @@ function ItemModal({ card, onClose }) {
   return (
     <div className="ItemModal-popup">
       <div className="ItemModal">
+        <img
+          className="ItemModal-close"
+          onClick={onClose}
+          src={closeX}
+          alt="help"
+        />
         <img className="ItemModal-image" src={src2} alt={alternate} />
         <p className="ItemModal-textItem"> {title}</p>
         <p className="ItemModal-textWeather">Weather: {weather}</p>
